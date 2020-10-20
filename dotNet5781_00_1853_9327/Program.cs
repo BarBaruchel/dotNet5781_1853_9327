5b4f05fea97ee88b9b2301d6e18f6ce360807e9c
BAR <BAR@DESKTOP-LJR9017>
2020-10-20 17:26:36 +0300

    first commit

---
+++ dotNet5781_00_1853_9327/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_1853_9327
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell Oh World!!");
+            Console.ReadKey();
+        }
+    }
+}
