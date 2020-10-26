e0613dd66ff9da6aee4d4b480e584c2a2d416aca
Ranit <ranit701@gmail.com>
2020-10-26 23:21:51 +0200

    almost done :)

--- dotNet5781_01_1853_9327/Bus.cs
+++ dotNet5781_01_1853_9327/Bus.cs
@@ -1,23 +1,86 @@
-﻿using System;
+﻿using dotNet5781_01_1853_9327;
+using System;
 
 namespace dotNet5781_01_1853_9327
 {
     public class Bus
     {
-        public int Kilometrage { get; set; }
+        public int Kilometrage { get; set; } = 0;
         private string rishuy;
+        private int delek = 1200;
 
-        public DateTime  Kaballa { get; set; }
+        public DateTime  Peilut { get; set; }
         public string Rishuy
         {
             get { return rishuy; }
-            set { rishuy = value; }
+            set {
+                rishuy = value;
+                    try
+                    {
+                        if (Peilut.Year < 2018)
+                        {
+                            if ((Rishuy[2] != '-') || (Rishuy[6] != '-'))
+                            {
+                            throw new Exception("location of makaf not in place");
+
+                            }
+                        }
+                    else
+                    {
+                        if ((Rishuy[3] != '-') || (Rishuy[6] != '-'))
+                        {
+                            throw new Exception("location of makaf not in place");
+
+                        }
+                    }
+
+                    }
+                    catch (Exception ex)
+                    {
+                    rishuy = null;
+                        throw new Exception(ex.Message);
+                    }
+                
+            }
         }
 
         public override string ToString()
         {
-            return string.Format("rishuy : {0}, \t kabala: {1}, KM: {2}",Rishuy,Kaballa,Kilometrage) 
+            return string.Format("rishuy : {0}, \t peilut: {1}, KM: {2}", Rishuy, Peilut, Kilometrage);
+        }
+        public void Bedika(int distance)
+        {
+           
+                if (delek - distance <= 0)
+                {
+                   throw new Exception("no enough fuel!");
+                }
+             if (Kilometrage+distance>=20000)
+            {
+                throw new Exception("need a treatment");
+            }
+             if (DateTime.Now.Subtract(Peilut).TotalDays>=365)
+            {
+                throw new Exception("need a treatment");
+            }
+            delek -= distance;
+            Kilometrage += distance;
+            
         }
 
+        public void tidluk()
+        {
+            delek = 0;
+        }
+
+        public void treatment()
+        {
+            Peilut = DateTime.Now;
+            Kilometrage = 0;
+        }
+        
     }
+   
 }
+
+
--- dotNet5781_01_1853_9327/Program.cs
+++ dotNet5781_01_1853_9327/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,30 +9,101 @@ namespace dotNet5781_01_1853_9327
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            bool exitFlag = false;
             List<Bus> chevra = new List<Bus>();
-            OPTION option;
             do
             {
                 Console.WriteLine(@" Make your choice:
-                                     ADD, FIND, EXIT = -1"); ;
-                string input = Console.ReadLine();
-                bool pass = Enum.TryParse(input,out  option);
-
+                     
+EXIT -> 0 ,ADD -> 1, FIND -> 2, print all the link buses-> 3, commit tidluk or treatment-> 4") ;
+                int option = Int32.Parse(Console.ReadLine());
+                
                 switch (option)
                 {
                     //TODO
-                    case OPTION.ADD:
+                    case 1:
+                        Bus newBus = new Bus();
+                        try
+                        {
+                            Console.WriteLine("please enter peliut date like this : Jan 1, 2009 ");
+                            newBus.Peilut = DateTime.Parse(Console.ReadLine());
+                            Console.WriteLine("please enter rishuy number");
+                            newBus.Rishuy = Console.ReadLine();
+                            
+                        }   catch(Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            break;
+                        };
+                        chevra.Add(newBus);
                         break;
-                    case OPTION.FIND:
+                    case 2:
+                        Console.WriteLine("please enter rishuy number");
+                        string requestBus = Console.ReadLine();
+                        var chosenBus = chevra.Find(bus => bus.Rishuy == requestBus);
+                            if (chosenBus == null)
+                        {
+                            Console.WriteLine("There is no such bus!");
+                            break;
+                        }
+                       int random = r.Next(1, 1200);
+                        Console.WriteLine(random);
+                        try
+                        {
+                            chosenBus.Bedika(random);
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            break;
+                        }
+
+
+
                         break;
-                    case OPTION.EXIT:
+                    case 3:
+                        foreach (var bus in chevra)
+                        {
+                            Console.WriteLine(bus);
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("please enter rishuy number");
+                         requestBus = Console.ReadLine();
+                         chosenBus = chevra.Find(bus => bus.Rishuy == requestBus);
+                        if (chosenBus == null)
+                        {
+                            Console.WriteLine("There is no such bus!");
+                            break;
+                        }
+                        Console.WriteLine("please enter 1 for tidluk or 2 for treatment");
+                        int input = Int32.Parse(Console.ReadLine());
+                        if (input==1)
+                        {
+                            chosenBus.tidluk();
+                        }
+                        else if (input == 2)
+                        {
+                            chosenBus.treatment();
+                        }
+                        break;
+                    case 0:
                         break;
                     default:
+                        Console.WriteLine("you press a wrong number ");
+                        //Console.WriteLine(@" Make your choice:
+                          //    EXIT -> 0 ,ADD -> 1, FIND -> 2, print all the link buses-> 3, commit tidluk or treatment-> ");
+                        exitFlag = true;
                         break;
                 }
-            } while (true);
+            } while (!exitFlag);
+              Console.ReadKey();
         }
+
     }
 }
